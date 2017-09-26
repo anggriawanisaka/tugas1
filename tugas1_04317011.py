bb5bfd5eec877fc1258f9656713fafd0dd03b4f0
anggriawanisaka <32089951+anggriawanisaka@users.noreply.github.com>
2017-09-26 14:39:07 +0700

    Add files via upload
    tugas alogaritma dan pemrogramman

---
+++ tugas1_04317011.py
@@ -0,0 +1,13 @@
+nama = ('Anggriawan Isaka Megantara')
+nim = ('04317011')
+umur = 18
+a = (umur + 5)
+
+print 'Nama : ' + nama
+print 'Nim : ' + nim
+print 'Umur : '
+print umur
+print 'Setelah 5 Tahun :'
+print (a)
+ 
+
